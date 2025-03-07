f4e974b0ec065c71bce82510fe5af3f2104cf042
cg <congvc98@gmail.com>
2025-03-07 23:48:50 +0700

    add more keys to socket events

--- src/socket-events.ts
+++ src/socket-events.ts
@@ -6,4 +6,7 @@ export const SocketEvents = {
   LOGGED_IN_USERS: 'logged-in-users',
   GET_LOGGED_IN_USERS: 'get-logged-in-users',
   REMOVE_LOGGED_IN_USERS: 'remove-logged-in-users',
+
+  USER_JOIN_ROOM: 'user-join-room',
+  USER_LEFT_ROOM: 'user-left-room',
 };
