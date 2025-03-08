1413a1a615c4aa2b3f0916a268b651a372cb6320
cg <congvc98@gmail.com>
2025-03-08 14:51:12 +0700

    update chat interface

--- src/chat.interface.ts
+++ src/chat.interface.ts
@@ -1,26 +1,45 @@
-import mongoose, { ObjectId } from 'mongoose';
+import mongoose from 'mongoose';
 
-export interface IConversationDocument {
+type ConversationType =
+  | mongoose.Types.ObjectId
+  | string
+  | string[]
+  | ILastMessage
+  | Date
+  | undefined;
+
+export type ILastMessage = {
+  messageId?: string;
+  senderId?: string;
+  body?: string;
+  createdAt?: Date | string;
+};
+
+export interface IConversationDocument
+  extends Record<string, ConversationType> {
   _id: mongoose.Types.ObjectId | string;
   conversationId: string;
   participants: string[];
-  lastMessage?: {
-    messageId?: string;
-    senderId?: string;
-    body?: string;
-    createdAt?: Date | string;
-  };
+  lastMessage?: ILastMessage;
   updatedAt?: Date | string;
-  [key: string]: string | {} | undefined;
+  [key: string]: ConversationType;
 }
 
-export interface IMessageDocument {
-  _id?: string | ObjectId;
+type MessageType =
+  | mongoose.Types.ObjectId
+  | string
+  | boolean
+  | ILastMessage
+  | Date
+  | undefined;
+
+export interface IMessageDocument extends Record<string, MessageType> {
+  _id?: mongoose.Types.ObjectId | string;
   conversationId?: string;
   body?: string;
   senderId?: string;
   receiverId?: string;
   isRead?: boolean;
   createdAt?: Date | string;
-  [key: string]: string | {} | undefined;
+  [key: string]: MessageType;
 }
