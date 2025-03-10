3b7de48f175998e286532ffc2b04f2bba0847c0b
cg <congvc98@gmail.com>
2025-03-10 21:25:13 +0700

    update order schema to nest payment method details

--- src/schemes/order.scheme.ts
+++ src/schemes/order.scheme.ts
@@ -2,13 +2,16 @@ import { PaymentMethod } from './../order.interface';
 import Joi, { ObjectSchema } from 'joi';
 
 const createOrderScheme: ObjectSchema = Joi.object().keys({
-  paymentMethod: Joi.string()
-    .valid(...Object.values(PaymentMethod))
-    .required()
-    .messages({
-      'any.required': 'Payment method is required',
-      'any.only': 'Invalid payment method',
-    }),
+  payment: Joi.object({
+    method: Joi.string()
+      .valid(...Object.values(PaymentMethod))
+      .required()
+      .messages({
+        'any.required': 'Payment method is required',
+        'any.only': 'Invalid payment method',
+      }),
+    transactionId: Joi.string().optional(),
+  }).required(),
 });
 
 export { createOrderScheme };
