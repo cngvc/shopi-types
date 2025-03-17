afdee47dec2c79037a437ad3115e89188325ea4e
cg <congvc98@gmail.com>
2025-03-17 21:39:07 +0700

    extend IEmailLocals interface with additional order details

--- src/email.interface.ts
+++ src/email.interface.ts
@@ -2,4 +2,12 @@ export interface IEmailLocals {
   username?: string;
   verifyLink?: string;
   resetLink?: string;
+  orderPublicId?: string;
+  totalAmount?: string;
+  shippingAddress?: string;
+  shippingCity?: string;
+  shippingCountry?: string;
+  orderLink?: string;
+  createdAt?: string;
+  [key: string]: string | undefined;
 }
